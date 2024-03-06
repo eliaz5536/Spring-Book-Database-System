35339c4a32cb17aa5d58c0310ec98cd68c0d82a0
Eliaz Simon <eliaz5536@gmail.com>
2024-03-06 19:55:27 +0000

    Add files via upload

---
+++ src/main/java/com/example/new_bookstore/NewBookstoreApplication.java
@@ -0,0 +1,11 @@
+package com.example.new_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBookstoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NewBookstoreApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/new_bookstore/models/Book.java
@@ -0,0 +1,41 @@
+package com.example.new_bookstore.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Book() {
+    }
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + ", id='" + id + '\'' + ", name=" + name + '}';
+    }
+}
